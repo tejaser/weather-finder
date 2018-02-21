f3823357f8247b1410e3144d6c52f82437dac01f
Tejas Pancholi <tejas.er@outlook.com>
2018-02-21 10:05:05 +0530

    make api connection and call

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = 'YOUR API KEY';
+
 class App extends React.Component {
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
